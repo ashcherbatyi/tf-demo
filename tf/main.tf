03ccc10609dac9e808ee585939dd7699313c3fe5
Alexey Shcherbatyi <alexey.shcherbatyi@gmail.com>
2024-08-08 20:15:16 +0000

    Add Terraform files

--- tf/main.tf
+++ tf/main.tf
@@ -1,15 +1,33 @@
-
+module "github_repository" {
+  source                   = "github.com/ashcherbatyi/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux0"
+}
 module "gke_cluster" {
-  source           = "github.com/ashcherbatyi/tf-google-gke-cluster"
-  GOOGLE_REGION    = var.GOOGLE_REGION
-  GOOGLE_PROJECT   = var.GOOGLE_PROJECT
-  GKE_NUM_NODES    = var.GKE_NUM_NODES
-  GKE_MACHINE_TYPE = var.GKE_MACHINE_TYPE
+  source         = "github.com/ashcherbatyi/tf-google-gke-cluster"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+}
+
+module "flux_bootstrap" {
+  source            = "github.com/ashcherbatyi/tf-fluxcd-flux-bootstrap"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  github_token      = var.GITHUB_TOKEN
+  private_key       = module.tls_private_key.private_key_pem
+  config_path       = module.gke_cluster.kubeconfig
+}
+
+module "tls_private_key" {
+  source    = "github.com/ashcherbatyi/tf-hashicorp-tls-keys"
+  algorithm = "RSA"
 }
 
 terraform {
   backend "gcs" {
-    bucket = "ashcherbatyi-demo-1"
-    prefix = "terraform/state"
+    bucket  = "ashcherbatyi-demo-1"
+    prefix  = "terraform/state"
   }
 }
--- tf/variables.tf
+++ tf/variables.tf
@@ -1,22 +1,33 @@
 variable "GOOGLE_PROJECT" {
   type        = string
+  default     = "demopj-430611"
   description = "GCP project name"
 }
 
 variable "GOOGLE_REGION" {
   type        = string
-  default     = "europe-west1"
+  default     = "europe-west1-b"
   description = "GCP region to use"
 }
 
-variable "GKE_NUM_NODES" {
-  type        = number
-  default     = 2
-  description = "Number of nodes in the GKE cluster"
+variable "GITHUB_OWNER" {
+  type        = string
+  description = "Github owner repositiry"
 }
 
-variable "GKE_MACHINE_TYPE" {
+variable "GITHUB_TOKEN" {
   type        = string
-  default     = "g1-small"
-  description = "Machine type"
+  description = "Github personal access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  default     = "flux-gitops"
+  description = "Flux GitOps repository"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type        = string
+  default     = "clusters"
+  description = "Flux manifest subdirectory"
 }
